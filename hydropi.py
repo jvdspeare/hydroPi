c9c18baf9bbe204bdec70a587cfc53e0a9b583e9
jvdspeare <jvdspeare@gmail.com>
2018-11-15 14:10:15 +0000

    Working on error handling

--- hydropi.py
+++ hydropi.py
@@ -1,62 +1,83 @@
-import configparser as config
-import pigpio as gpio
-# import DHT22
-import pymysql as sql
-import time
-from multiprocessing import Process
+# import modules
+try:
+    import configparser as config
+    import pigpio as gpio
+    import DHT22
+    import pymysql as sql
+    import time
+    from multiprocessing import Process
+    import warnings
+    warnings.filterwarnings('error')
+except ModuleNotFoundError as er:
+    quit(print(er))
 
 
 # load config file
 def get_conf(file_name):
-    get_conf.conf = config.ConfigParser()
-    get_conf.conf.read(file_name)
+    try:
+        get_conf.conf = config.ConfigParser()
+        get_conf.conf.read(file_name)
+    except config.ParsingError as e:
+        quit(print(e))
 
 
 # connect to database and check connectivity
-def sql_db_connect(host, user, passw, db_name, error_msg):
+def sql_db_connect(host, user, passw, db_name, db_table):
     try:
         sql_db_connect.db = sql.connect(host, user, passw, db_name)
-    except:
-        print(error_msg)
+    except sql.err.OperationalError as e:
+        quit(print(e))
+    try:
+        cursor = sql_db_connect.db.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS %s (
+           TIME INT NOT NULL,
+           TEMP FLOAT(3, 1) NOT NULL,
+           HUMID FLOAT(3, 1) NOT NULL)''' % db_table)
+    except Warning as w:
+        print(w)
 
 
 # setup temperature and humidity sensor
-def setup_temp_humid(gpio_num, error_msg):
+def setup_temp_humid(gpio_num):
     try:
         pi = gpio.pi()
+        if not pi.connected:
+            quit()
         dht22 = DHT22.sensor(pi, gpio_num)
         dht22.trigger()
-    except:
-        print(error_msg)
+        time.sleep(3)
+    except AttributeError as e:
+        quit(print(e))
 
 
 # take temperature and humidity reading then store data in database
-def get_temp_humid(error_msg):
+def get_temp_humid(db_table):
     while True:
         try:
-            dht22.trigger()
-            temp = dht22.temperature()
-            humid = dht22.humidity()
+            # dht22.trigger()
+            # temp = dht22.temperature()
+            # humid = dht22.humidity()
+            print(time.time())
+            temp = 21.222
+            humid = 75.222
             cursor = sql_db_connect.db.cursor()
-            statement = "INSERT INTO TEMP_HUMID(TIME, TEMP, HUMID) VALUES ('%s', %d, %d)" % (time.time(), temp, humid)
-            print(statement)
-            cursor.execute(statement)
+            q = "INSERT INTO %s(TIME, TEMP, HUMID) VALUES (%d, %f, %f)" % (db_table, time.time(), temp, humid)
+            cursor.execute(q)
             sql_db_connect.db.commit()
             print('done')
-            time.sleep(600)
-        except:
-            print(error_msg)
-            time.sleep(600)
+            time.sleep(int(get_conf.conf['SENSOR']['TEMP_HUMID_FREQ']))
+        except ValueError as e:
+            quit(print(e))
 
 
 # load config, connect to the database and setup the sensor(s)
 get_conf('config.ini')
 sql_db_connect(get_conf.conf['DB']['HOST'], get_conf.conf['DB']['USER'], get_conf.conf['DB']['PASSW'],
-               get_conf.conf['DB']['DB_NAME'], 'Database Error222')
-# setup_temp_humid(get_conf.conf['SENSOR']['TEMP_HUMID_GPIO'], 'Sensor Error')
+               get_conf.conf['DB']['DB_NAME'], get_conf.conf['DB']['DB_TABLE'])
+setup_temp_humid(int(get_conf.conf['SENSOR']['TEMP_HUMID_GPIO']))
 
 # start a process to run the get_temp_humid function, this will take temperature and humidity readings every x time
-Process(target=get_temp_humid('Sensor Error')).start()
+Process(target=get_temp_humid(get_conf.conf['DB']['DB_TABLE'])).start()
 
 # close database connection
 sql_db_connect.db.close()
