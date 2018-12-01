08af956d9d76fb6b097a3df3b577282739a826d2
jvdspeare <jvdspeare@gmail.com>
2018-12-01 21:24:22 +0000

    Application

--- hydropi.py
+++ hydropi.py
@@ -90,7 +90,7 @@ def get_soil_moisture(ch, db_table, freq):
     while True:
         for i in ch:
             print(i)
-            data = setup_soil_moisture.mcp.read_adc(i)
+            data = setup_soil_moisture.mcp.read_adc(int(i))
             cursor = sql_db_connect.db.cursor()
             cursor.execute('INSERT INTO %s(TIME, CH%s) VALUES (%f, %s)' %
                            (db_table, i, time.time(), data))
